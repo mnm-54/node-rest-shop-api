36e63eeb165a370aa447ad4e8f567654f7ecdd12
Munem <shahriarmun54@gmail.com>
2021-06-04 00:53:25 +0600

    day4 update

--- app.js
+++ app.js
@@ -1,45 +1,58 @@
-const express= require('express')
-const app= express()
-const morgan= require('morgan')
-const bodyParser= require('body-parser')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const productRoutes= require('./api/routes/products')
-const orderRoutes= require('./api/routes/orders')
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+//conenting mongoose
+mongoose.connect(
+  "mongodb+srv://testboy:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0.cjcrz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 //handling CORS error
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header(
-        'Access-Control-Allow-Headers'
-        ,'Origon, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method==='OPTIONS'){
-        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next()
-})
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origon, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // use is a middleware
-app.use('/products',productRoutes)
-app.use('/orders',orderRoutes)
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
-app.use((req,res,next)=>{
-    const error= new Error('Not found');
-    error.status=404
-    next(error)
-})
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
-app.use((error,req,res,next)=>{
-    res.status(error.status || 500)
-    res.json({
-        error:{
-            message: error.message
-        }
-    });
-})
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
-module.exports=app;
+module.exports = app;
