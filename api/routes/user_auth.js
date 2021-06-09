3f0430508866ee35bd25f3a3a24f156048cfa382
Munem <shahriarmun54@gmail.com>
2021-06-09 23:41:11 +0600

    day6 update

--- api/routes/user_auth.js
+++ api/routes/user_auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -43,6 +44,48 @@ router.post("/signup", (req, res, next) => {
       }
     })
     .catch();
+});
+
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        res.status(404).json({
+          message: "Auth fail",
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          res.status(404).json({
+            message: "Auth fail",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "auth successful",
+            token: token,
+          });
+        }
+        res.status(404).json({
+          message: "Auth fail",
+        });
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+      console.log(err);
+    });
 });
 
 router.delete("/:userId", (req, res, next) => {
